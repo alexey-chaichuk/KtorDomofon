fdd181c21e1891c41ce37337d6a11dd69b64f607
alexey.chaichuk <alexey.chaichuk@gmail.com>
2023-09-01 03:34:11 +0500

    added dialog to change door name, fixed swipe actions for camera

--- app/src/main/java/ru/cpro/ktordomofon/ui/CamerasScreen.kt
+++ app/src/main/java/ru/cpro/ktordomofon/ui/CamerasScreen.kt
@@ -1,43 +1,57 @@
 package ru.cpro.ktordomofon.ui
 
+import android.util.Log
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.DismissDirection
-import androidx.compose.material.DismissValue
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FractionalThreshold
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.Text
-import androidx.compose.material.rememberDismissState
+import androidx.compose.material.rememberSwipeableState
+import androidx.compose.material.swipeable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import kotlinx.coroutines.launch
 import ru.cpro.ktordomofon.R
 import ru.cpro.ktordomofon.ui.viewmodel.MainViewModel
 import ru.cpro.ktordomofon.ui.viewmodel.MainViewUiState
+import kotlin.math.roundToInt
+
+private enum class SwipeStateCamera {
+    On,
+    Off
+}
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -70,26 +84,37 @@ fun CamerasScreen(
                 Spacer(modifier = Modifier.height(8.dp))
 
                 for (camera in room.second) {
+                    val scope = rememberCoroutineScope()
                     var fav by remember { mutableStateOf(camera.favorites) }
-                    val dismissState = rememberDismissState(
-                        confirmStateChange = {
-                            if (it == DismissValue.DismissedToStart || it == DismissValue.DismissedToEnd) {
-                                 fav = !fav
-                                //TODO save favorites for item by vm
-                                false
-                            } else false
-                        }
-                    )
+                    val swipeState = rememberSwipeableState(SwipeStateCamera.Off)
+                    val sizePx = with(LocalDensity.current) { 80.dp.toPx() }
 
-                    SwipeToDismiss(state = dismissState,
-                        directions = setOf(DismissDirection.EndToStart),
-                        background = {
-
-                            Box(
-                                Modifier
-                                    .fillMaxSize()
-                                    .padding(horizontal = 20.dp),
-                                contentAlignment = Alignment.CenterEnd
+                    Box(
+                        modifier = Modifier
+                            .swipeable(
+                                state = swipeState,
+                                anchors = mapOf(
+                                    0f to SwipeStateCamera.Off,
+                                    -sizePx to SwipeStateCamera.On,
+                                ),
+                                thresholds = { _, _ -> FractionalThreshold(0.5f) },
+                                orientation = Orientation.Horizontal
+                            ),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Row(
+                            Modifier
+                                .padding(horizontal = 16.dp)
+                                .align(Alignment.CenterEnd)
+                        ) {
+                            IconButton(
+                                onClick = {
+                                    Log.d("CameraScreen", "star btn clicked")
+                                    fav = !fav
+                                    scope.launch {
+                                        swipeState.animateTo(SwipeStateCamera.Off, tween(600, 0))
+                                    }
+                                }
                             ) {
                                 Icon(
                                     painterResource(id = R.drawable.star),
@@ -97,62 +122,70 @@ fun CamerasScreen(
                                     contentDescription = null
                                 )
                             }
-                        },
-                        dismissContent = {
-                            Card {
-                                Column {
-                                    Box(modifier = Modifier.fillMaxWidth()) {
-                                        AsyncImage(
-                                            model = camera.snapshot,
-                                            contentDescription = null,
-                                            contentScale = ContentScale.FillWidth,
-                                            modifier = Modifier
-                                                .fillMaxWidth()
+                        }
+                        Card(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp)
+                                .offset {
+                                    IntOffset(swipeState.offset.value.roundToInt(), 0)
+                                },
+                            elevation = 2.dp,
+                            shape = RoundedCornerShape(12.dp)
+                        ) {
+                            Column {
+                                Box(modifier = Modifier.fillMaxWidth()) {
+                                    AsyncImage(
+                                        model = camera.snapshot,
+                                        contentDescription = null,
+                                        contentScale = ContentScale.FillWidth,
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                    )
+
+                                    Row(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .padding(16.dp)
+                                    ) {
+
+                                        val recText = if (camera.rec) "REC" else ""
+                                        Text(
+                                            text = recText,
+                                            color = Color.Red,
+                                            modifier = Modifier.weight(1f)
                                         )
 
-                                        Row(
-                                            modifier = Modifier
-                                                .fillMaxWidth()
-                                                .padding(16.dp)
-                                        ) {
-
-                                            val recText = if (camera.rec) "REC" else ""
-                                            Text(
-                                                text = recText,
-                                                color = Color.Red,
-                                                modifier = Modifier.weight(1f)
-                                            )
-
-                                            if (fav) {
-                                                Icon(
-                                                    painterResource(id = R.drawable.star_filled),
-                                                    tint = colorResource(R.color.gold_star),
-                                                    contentDescription = null
-                                                )
-                                            }
-                                        }
-
-                                        Box(
-                                            modifier = Modifier.matchParentSize(),
-                                            contentAlignment = Alignment.Center
-                                        ) {
+                                        if (fav) {
                                             Icon(
-                                                painterResource(R.drawable.play_button),
-                                                contentDescription = null,
-                                                tint = Color.White,
-                                                modifier = Modifier.size(60.dp)
+                                                painterResource(id = R.drawable.star_filled),
+                                                tint = colorResource(R.color.gold_star),
+                                                contentDescription = null
                                             )
                                         }
                                     }
 
-                                    Text(
-                                        modifier = Modifier.padding(16.dp),
-                                        text = camera.name,
-                                        style = MaterialTheme.typography.body1
-                                    )
+                                    Box(
+                                        modifier = Modifier.matchParentSize(),
+                                        contentAlignment = Alignment.Center
+                                    ) {
+                                        Icon(
+                                            painterResource(R.drawable.play_button),
+                                            contentDescription = null,
+                                            tint = Color.White,
+                                            modifier = Modifier.size(60.dp)
+                                        )
+                                    }
                                 }
+
+                                Text(
+                                    modifier = Modifier.padding(16.dp),
+                                    text = camera.name,
+                                    style = MaterialTheme.typography.body1
+                                )
                             }
-                        })
+                        }
+                    }
 
                     Spacer(modifier = Modifier.height(8.dp))
                 }
--- app/src/main/java/ru/cpro/ktordomofon/ui/DoorsScreen.kt
+++ app/src/main/java/ru/cpro/ktordomofon/ui/DoorsScreen.kt
@@ -1,30 +1,40 @@
 package ru.cpro.ktordomofon.ui
 
-import android.util.Log
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.FractionalThreshold
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.material.rememberSwipeableState
 import androidx.compose.material.swipeable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -34,13 +44,14 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import coil.compose.AsyncImage
 import kotlinx.coroutines.launch
 import ru.cpro.ktordomofon.R
 import ru.cpro.ktordomofon.ui.viewmodel.MainViewUiState
 import kotlin.math.roundToInt
 
-private enum class SwipeState{
+private enum class SwipeStateDoor{
     On,
     Off
 }
@@ -49,29 +60,58 @@ private enum class SwipeState{
 @Composable
 fun DoorsScreen(uiState: State<MainViewUiState>,
                 modifier: Modifier = Modifier) {
+    var isChangeNameDialogVisible by remember { mutableStateOf(false) }
+    var textToChange by remember { mutableStateOf("") }
+    var doorIdToChange by remember { mutableStateOf(0) }
+
+    if(isChangeNameDialogVisible) {
+        EditNameDialog(
+            content = {
+                OutlinedTextField(
+                    value = textToChange,
+                    onValueChange = { textToChange = it },
+                    label = { Text("Name") },
+                )
+            },
+            onDismiss = { isChangeNameDialogVisible = false }
+        ) {
+            isChangeNameDialogVisible = false
+        }
+    }
+
     LazyColumn(modifier = modifier
         .background(MaterialTheme.colors.background)
         .padding(top = 16.dp)
     ) {
         items(uiState.value.doors) { door ->
             val scope = rememberCoroutineScope()
-            val swipeState = rememberSwipeableState(SwipeState.Off)
+            val swipeState = rememberSwipeableState(SwipeStateDoor.Off)
             val sizePx = with(LocalDensity.current) { 120.dp.toPx() }
 
-            Box {
+            Box(modifier = Modifier
+                .swipeable(
+                    state = swipeState,
+                    anchors = mapOf(
+                        0f to SwipeStateDoor.Off,
+                        -sizePx to SwipeStateDoor.On,
+                    ),
+                    thresholds = { _, _ -> FractionalThreshold(0.5f) },
+                    orientation = Orientation.Horizontal
+                ),
+                contentAlignment = Alignment.Center
+            ) {
                 Row(modifier = Modifier
                     .align(Alignment.CenterEnd)
                     .padding(16.dp)
                 ) {
                     IconButton(
                         onClick = {
-                            Log.d("DoorScreen", "edit btn clicked")
+                            textToChange = door.name
+                            isChangeNameDialogVisible = true
                             scope.launch {
-                                swipeState.animateTo(SwipeState.Off, tween(600, 0))
+                                swipeState.animateTo(SwipeStateDoor.Off, tween(600, 0))
                             }
-                        },
-                        enabled = true,
-
+                        }
                     ) {
                         Icon(
                             painter = painterResource(id = R.drawable.edit),
@@ -83,7 +123,7 @@ fun DoorsScreen(uiState: State<MainViewUiState>,
                     IconButton(
                         onClick = {
                             scope.launch {
-                                swipeState.animateTo(SwipeState.Off, tween(600, 0))
+                                swipeState.animateTo(SwipeStateDoor.Off, tween(600, 0))
                             }
                         }
                     ) {
@@ -99,18 +139,11 @@ fun DoorsScreen(uiState: State<MainViewUiState>,
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(8.dp)
-                        .swipeable(
-                            state = swipeState,
-                            anchors = mapOf(
-                                0f to SwipeState.Off,
-                                -sizePx to SwipeState.On,
-                            ),
-                            thresholds = { _, _ -> FractionalThreshold(0.5f) },
-                            orientation = Orientation.Horizontal
-                        )
                         .offset {
                             IntOffset(swipeState.offset.value.roundToInt(), 0)
-                        }
+                        },
+                    elevation = 2.dp,
+                    shape = RoundedCornerShape(12.dp)
                 ) {
                     Column {
                         door.snapshot?.let {
@@ -152,6 +185,41 @@ fun DoorsScreen(uiState: State<MainViewUiState>,
                             )
                         }
                     }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun EditNameDialog(
+    content: @Composable () -> Unit,
+    onDismiss: () -> Unit,
+    onSave: () -> Unit,
+) {
+    Dialog(onDismiss) {
+        Surface(shape = MaterialTheme.shapes.medium) {
+            Column {
+                Column(Modifier.padding(24.dp)) {
+                    Text(text = "Change name")
+                    Spacer(Modifier.size(16.dp))
+                    content.invoke()
+                }
+                Spacer(Modifier.height(4.dp))
+                Row(
+                    Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(),
+                    Arrangement.spacedBy(8.dp, Alignment.End),
+                ) {
+                    TextButton(
+                        onClick = { onDismiss.invoke() },
+                        content = { Text("CANCEL") },
+                    )
+                    TextButton(
+                        onClick = { onSave.invoke() },
+                        content = { Text("OK") },
+                    )
                 }
             }
         }
